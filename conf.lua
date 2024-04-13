ec267b8e2dd79bc54831bf6748d38ea6683ba29e
Henry Denny <henry.denny12@gmail.com>
2024-04-13 18:01:22 +0100

    Make variable more descriptive.

--- conf.lua
+++ conf.lua
@@ -1,16 +1,16 @@
 -- INFO: This is a special configuration file that is run before the other LÖVE modules are loaded.
-function love.conf(t)
+function love.conf(app)
 	-- Version of the game
 	-- t.version = "1.0.0"
 
 	-- Set window properties
-	t.window.title = "<GAME NAME>"
-	t.window.width = 1280
-	t.window.height = 720
-	t.window.resizable = false
+	app.window.title = "<GAME NAME>"
+	app.window.width = 1280
+	app.window.height = 720
+	app.window.resizable = false
 
 	-- Remove unused modules (reduces startup time)
-	t.modules.physics = false
-	t.modules.joystick = false
-	t.modules.touch = false
+	app.modules.physics = false
+	app.modules.joystick = false
+	app.modules.touch = false
 end
